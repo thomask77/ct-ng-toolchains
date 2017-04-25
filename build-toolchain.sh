b7871ed0341c02d8fb2b4851e6475fdd94c0f58e
Thomas Kindler <mail_git@t-kindler.de>
2017-04-25 22:57:56 +0200

    Updated to crosstool-ng-1.23.0 Enable link-time optimization support Generate PDF manuals

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -9,15 +9,13 @@ CT_NG=$BASE_DIR/crosstool-ng/ct-ng
 
 function build {
     CONFIG_DIR=$BASE_DIR/$1
-
-    mkdir -p $DOWNLOADS_DIR
-    mkdir -p $RELEASES_DIR
-
     echo
     echo "***** Building $1 *****"
     echo
 
-    cd $CONFIG_DIR
+    mkdir -p "$DOWNLOADS_DIR"
+
+    cd "$CONFIG_DIR"
     if [[ ! -f defconfig ]]; then
         echo "ERROR: $CONFIG_DIR/defconfig not found" 1>&2
         exit 1
@@ -26,27 +24,45 @@ function build {
     $CT_NG defconfig
     $CT_NG clean
     $CT_NG build
+}
 
+
+function package {
+    CONFIG_DIR=$BASE_DIR/$1
     echo
     echo "***** Packaging $1 *****"
     echo
 
-    cd $CONFIG_DIR/output
+    mkdir -p "$RELEASES_DIR"
 
     # The output directory name is defined by the defconfig and
     # includes the GCC version number (which we don't know yet).
     #
     # So we have to search the output directory.
     #
-    for REL_NAME in *; do
-        if [[ -d $REL_NAME ]]; then
-            REL_HOST=${1/*-/}   # win64 or linux
-            REL_DATE=$(date +%Y%m%d -r$REL_NAME)
+    cd "$CONFIG_DIR/output"
 
-            # h - do not create symlinks (windows can't extract them)
-            #
-            tar chvJf $RELEASES_DIR/$REL_NAME-$REL_HOST-$REL_DATE.tar.xz $REL_NAME
-        fi
+    for REL_NAME in gcc-*; do
+        if [[ ! -d $REL_NAME ]]; then continue; fi
+
+        REL_HOST="${1/*-/}"   # win64 or linux
+        REL_DATE=$(date +%Y%m%d -r"$REL_NAME")
+
+        # Keep PDF documentation only and move to one directory
+        #
+        chmod -R +w "$REL_NAME/share"
+        find "$REL_NAME/share/doc" -type f -not -name "*.pdf" -delete
+        find "$REL_NAME/share/doc" -type f -exec mv "{}" "$REL_NAME/share/doc" ";"
+        find "$REL_NAME/share/doc" -type d -empty -delete
+        rm -rf "$REL_NAME/share/info"
+        rm -rf "$REL_NAME/share/man"
+        chmod -R -w "$REL_NAME/share"
+
+        # --dereference because windows can't handle symlinks
+        #
+        tar --create --verbose --dereference --xz \
+            --file "$RELEASES_DIR/$REL_NAME-$REL_HOST-$REL_DATE.tar.xz" \
+            "$REL_NAME"
     done
 }
 
@@ -59,5 +75,6 @@ fi
 
 for ARG in "$@"; do
     build "$ARG"
+    package "$ARG"
 done
 
